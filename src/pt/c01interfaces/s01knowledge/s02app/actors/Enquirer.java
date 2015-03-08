340e1aacac8737b7ddfebc9150127fcdbb20cc8e
Miguel Nicodemus <miguelnicodemus36@gmail.com>
2015-03-08 20:28:03 -0300

    Terminado trabalho

--- src/pt/c01interfaces/s01knowledge/s02app/actors/Enquirer.java
+++ src/pt/c01interfaces/s01knowledge/s02app/actors/Enquirer.java
@@ -9,41 +9,72 @@ import pt.c01interfaces.s01knowledge.s01base.inter.IResponder;
 
 public class Enquirer implements IEnquirer
 {
-    IObjetoConhecimento obj;
-	
-	public Enquirer()
-	{
-	}
-	
+	IObjetoConhecimento obj;
 	
 	@Override
 	public void connect(IResponder responder)
 	{
-        IBaseConhecimento bc = new BaseConhecimento();
+		IBaseConhecimento bc = new BaseConhecimento();
+		String animals[] = bc.listaNomes();
+		String dbQandA[][] = new String[20][2];
 		
-		obj = bc.recuperaObjeto("tiranossauro");
-
-		IDeclaracao decl = obj.primeira();
-		
-        boolean animalEsperado = true;
-		while (decl != null && animalEsperado) {
-			String pergunta = decl.getPropriedade();
-			String respostaEsperada = decl.getValor();
-			
-			String resposta = responder.ask(pergunta);
-			if (resposta.equalsIgnoreCase(respostaEsperada))
-				decl = obj.proxima();
-			else
-				animalEsperado = false;
+		for (int i = 0; i < 20; i++) {
+			for (int j = 0; j < 2; j++) {
+				dbQandA[i][j] = "DEFAULT";
+			}
 		}
 		
-		boolean acertei = responder.finalAnswer("tiranossauro");
-		
-		if (acertei)
-			System.out.println("Oba! Acertei!");
-		else
-			System.out.println("fuem! fuem! fuem!");
-
+		for (int i = 0; i < animals.length; i++) {
+			obj = bc.recuperaObjeto(animals[i]);
+			IDeclaracao decl = obj.primeira();
+			boolean animalEsperado = true;
+			
+			while (decl != null && animalEsperado) {
+				String pergunta = decl.getPropriedade();
+				String respostaEsperada = decl.getValor();
+				
+				boolean questionFound = false;
+				
+				int k = 0;
+				
+				for (k = 0; k < 20 && !questionFound; k++) {
+					if (dbQandA[k][0].equalsIgnoreCase(pergunta)) {
+						questionFound = true;
+					}
+				}
+				
+				if (!questionFound) {
+					String resposta = responder.ask(pergunta);
+					
+					int j = 0;
+					while (j < 100 && !dbQandA[j][1].equalsIgnoreCase("DEFAULT")) {
+						j++;
+					}
+					
+					dbQandA[j][0] = pergunta;
+					dbQandA[j][1] = resposta;
+					
+					if (resposta.equalsIgnoreCase(respostaEsperada))
+						decl = obj.proxima();
+					else
+						animalEsperado = false;
+				
+				} else {
+					if (dbQandA[k][1].equalsIgnoreCase(respostaEsperada)) 
+						decl = obj.proxima();
+					else
+						animalEsperado = false;
+				}
+			}
+			
+			if (animalEsperado) {
+				boolean acertei = responder.finalAnswer(animals[i]);
+				
+				if (acertei)
+					System.out.println("Oba! Acertei!");
+				else
+					System.out.println("fuem! fuem! fuem!");
+			}
+		}
 	}
-
 }
